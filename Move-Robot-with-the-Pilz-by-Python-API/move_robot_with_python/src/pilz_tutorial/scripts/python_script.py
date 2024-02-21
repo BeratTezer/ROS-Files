01a046c703e8bdb5395aa5582377327878be9eb8
Berat Tezer <tezerberat42@gmail.com>
2024-02-21 15:42:43 +0300

    Upload 21.2.24

--- Move-Robot-with-the-Pilz-by-Python-API/move_robot_with_python/src/pilz_tutorial/scripts/python_script.py
+++ Move-Robot-with-the-Pilz-by-Python-API/move_robot_with_python/src/pilz_tutorial/scripts/python_script.py
@@ -12,6 +12,17 @@ __ROBOT_VELOCITY__ = 0.5        # velocity of the robot
 def start_program():
     print(r.get_current_pose()) # print the current position of thr robot in the terminal
 
+    # define the positions:
+    joint_goal = 150 # Use joint values for the first position
+    cartesian_goal = 150 # Use cartesian coordinates for another position
+    
+    # Move to start point with joint values to avoid random trajectory
+    r.move(Ptp(goal=joint_goal, vel_scale=__ROBOT_VELOCITY__))
+
+    #Move to the second position
+    r.move(Ptp(goal=cartesian_goal, vel_scale=__ROBOT_VELOCITY__))
+
+
 if __name__ == "__main__":
     # init a rosnode
     rospy.init_node('robot_program_node')
